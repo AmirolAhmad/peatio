b83f03fdf75d57a3d958d4595c223cbd7f5badb9
AmirolAhmad <amirol@lizard-apps.com>
2018-02-19 13:51:37 +0800

    add the linked files for capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :repo_url, "git@github.com:AmirolAhmad/peatio.git"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml", "config/currencies.yml", "config/application.yml", "config/banks.yml", "config/markets.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
